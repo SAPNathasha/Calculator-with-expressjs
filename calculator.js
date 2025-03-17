83d5f90e4fa68f44d5b87437dcef6e7ae06664ef
SAPNathasha <131260315+SAPNathasha@users.noreply.github.com>
2025-03-17 10:38:50 +0530

    Create responses to requests (Strings type)

--- calculator.js
+++ calculator.js
@@ -1,7 +1,9 @@
 //jshint esversion:6
 
 const express = require("express");
+const bodyPasser = require("body-parser");
 const app = express();
+app.use(bodyPasser.urlencoded({extended: true}));
 
 app.listen("3000", function(){
     console.log("Starting in port 3000");
@@ -9,4 +11,11 @@ app.listen("3000", function(){
 
 app.get("/", function(req , res){
     res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/" , function(req , res){
+    var first = req.body.num1;
+    var second = req.body.num2;
+    var result = first+second;
+    res.send("The result is: " + result);
 });
